3ac2ddb9907cfb32726f197014c077d4c14d81a4
Anirudh <73699651+AnirudhN1508@users.noreply.github.com>
2021-09-22 18:16:07 +0530

    Info.js update

--- cs8-learning/src/Info.js
+++ cs8-learning/src/Info.js
@@ -4,14 +4,36 @@ const Info = () =>{
             <div>
                 <div className="about-page">
                     <div className="container">
-                    <div className="about-message">
-                        <Icon />
-                    <h1>Info Page</h1>
-                    <p>This is about the website and our misson and etc.
-                        Aute dolor enim officia mollit minim Lorem sit voluptate. In duis consectetur et reprehenderit occaecat consequat sit minim ullamco. Sint sint sunt enim ullamco nisi ad officia nulla irure consectetur in laborum. Nostrud aute in commodo consectetur tempor dolore proident magna esse est enim incididunt. Occaecat irure reprehenderit cupidatat eiusmod aute.
-                    </p>
+                        <div className="about-message">
+                            <Icon />
+                        <h1>Info Page</h1>
+                        <p>This is about the website and our misson and etc.
+                            Labore proident eu sunt mollit aliquip ea sunt minim duis eiusmod commodo proident eu eiusmod.
+                        </p>
+                        </div>
+                        <div className="about-us">
+                            <header>About Us</header>
+                            <p>
+                                Fugiat sint do eu cupidatat ea commodo tempor est aliqua deserunt magna aliquip. Sit cillum culpa ea exercitation duis ipsum adipisicing. Tempor est mollit dolor elit magna eiusmod mollit excepteur ullamco exercitation.
+                            </p>
+                        </div>
+                        <div className="about-us">
+                            <header>Our Goal</header>
+                            <p>
+                                Amet sint duis cillum eu culpa commodo in reprehenderit. Elit aute consectetur nisi ullamco sunt Lorem. Dolor ullamco ipsum eu sint Lorem culpa eu quis sit in aliquip duis enim. Id exercitation consequat anim ad fugiat dolor.
+                            </p>
+                        </div>
+                        <div className="about-us">
+                            <header>Something</header>
+                            <p>
+                                Et aute magna eiusmod cillum sint velit mollit occaecat ex non veniam eiusmod. Ipsum nostrud eu dolor occaecat elit mollit. Officia laboris id aliqua sit fugiat. Do dolor sint mollit ex aute quis sunt mollit aliqua labore cupidatat. Nulla pariatur velit pariatur aliquip adipisicing magna in nisi adipisicing deserunt officia id.
+                            </p>
+                        </div>
+                        <div className="btn-grp">
+                        <a href="mailto:amigos@gmail.com"><button className="abt-btn">Contact Us</button></a>
+                        </div>
                     </div>
-                    </div>
+                    
                 </div>
             </div>
         );
