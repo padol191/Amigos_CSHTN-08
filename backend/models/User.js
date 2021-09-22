3fd5a7793f123a466b60f12a75339160f0eb41e5
chaitanya <chaitanyapadol@icloud.com>
2021-09-22 14:18:56 +0530

    user registration api added

---
+++ backend/models/User.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  assignment: [
+    {
+      title: {
+        type: Schema.Types.String,
+        ref: "assignment",
+      },
+      description: {
+        type: Schema.Types.String,
+        ref: "assignment",
+      },
+      completed: {
+        type: Boolean,
+      },
+      fileUrl: {
+        type: String,
+      },
+      subject: {
+        type: String,
+      },
+    },
+  ],
+});
+
+module.exports = User = mongoose.model("user", UserSchema);
