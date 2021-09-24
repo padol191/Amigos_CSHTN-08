c70a9117ec4fccd24a3707e1d71ce47363164776
aaryan <aaryanpatil812@yahoo.com>
2021-09-24 16:08:33 +0530

    NavbarLoggedIn Added

--- cs8-learning/src/Components/HomeUI/Home.js
+++ cs8-learning/src/Components/HomeUI/Home.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import NavBar from './Navbar';
 import Info from './Info';
-import Footer from './Footer';
 import { Link } from 'react-router-dom';
 
 const Home=()=> {
     return (
+      
       <div style={{height:'100%'}}>
         <Info />
       </div>
